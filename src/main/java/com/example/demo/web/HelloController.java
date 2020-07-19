4b457de9554a62c8c92b267695c87d436e9a9831
zhanglu <1113273910@qq.com>
2020-07-19 21:15:46 +0800

    添加端口号显示

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by SFI on 2020/7/3.
  */
 @RestController
+
 public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
 
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        String msg = "端口号："+port;
+        return msg;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
     }
 
 }
